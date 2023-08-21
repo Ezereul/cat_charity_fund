a592b2f1325081420ac1b832da805ce3e203edc4
Isaev <isaev_0602@mail.ru>
2023-08-21 12:17:03 +0500

    Инвестирование без лишних проходов цикла, исправлено аннотирование.

--- app/services/investing.py
+++ app/services/investing.py
@@ -1,23 +1,20 @@
 from datetime import datetime
-from typing import List, Protocol
+from typing import List
 
-
-class InvestmentUnit(Protocol):
-    full_amount: int
-    invested_amount: int
-    fully_invested: bool
-    close_date: datetime
+from app.models.investment_base import InvestmentBase
 
 
 def distribute_investment(
-        source: InvestmentUnit,
-        targets: List[InvestmentUnit]) -> List[InvestmentUnit]:
+        source: InvestmentBase,
+        targets: List[InvestmentBase]) -> List[InvestmentBase]:
     changed_targets = []
     for target in targets:
         investment = min(
             source.full_amount - source.invested_amount,
             target.full_amount - target.invested_amount
         )
+        if investment == 0:
+            break
         for unit in [source, target]:
             unit.invested_amount += investment
             if unit.full_amount == unit.invested_amount:
